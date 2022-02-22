2bab0bdc70b1a6802ac4388ee9bd7e1fd2cf2cc3
Ryan Winter <ryanwinter@outlook.com>
2022-02-21 16:53:19 -0800

    cleanup cortex file naming

--- arm-cortex-gcc/arm-gcc-cortex-m4.cmake
+++
@@ -1,8 +0,0 @@
-# Define the CPU architecture for Threadx
-set(THREADX_ARCH "cortex_m4")
-set(THREADX_TOOLCHAIN "gnu")
-
-set(MCPU_FLAGS "-mthumb -mcpu=cortex-m4")
-set(VFP_FLAGS "-mfloat-abi=hard -mfpu=fpv4-sp-d16")
-
-include(${CMAKE_CURRENT_LIST_DIR}/arm-gcc-cortex-toolchain.cmake)
--- arm-cortex-gcc/arm-gcc-cortex-m7.cmake
+++
@@ -1,8 +0,0 @@
-# Define the CPU architecture for Threadx
-set(THREADX_ARCH "cortex_m7")
-set(THREADX_TOOLCHAIN "gnu")
-
-set(MCPU_FLAGS "-mthumb -mcpu=cortex-m7")
-set(VFP_FLAGS "-mfloat-abi=hard -mfpu=fpv5-sp-d16")
-
-include(${CMAKE_CURRENT_LIST_DIR}/arm-gcc-cortex-toolchain.cmake)
