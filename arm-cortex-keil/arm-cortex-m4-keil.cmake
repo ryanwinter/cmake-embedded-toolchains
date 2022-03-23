962c8ac6efda7c0a3801c0847ecc87d24afd049a
Ryan Winter <ryanwinter@outlook.com>
2022-03-22 23:39:49 -0700

    update keil toolchain

--- arm-cortex-keil/arm-cortex-m4-keil.cmake
+++ arm-cortex-keil/arm-cortex-m4-keil.cmake
@@ -1,6 +1,7 @@
 # Compiler flags needed to compile for this CPU
 #set(CMAKE_SYSTEM_PROCESSOR cortex-m4)
 #set(CMAKE_SYSTEM_ARCH armv7e-m)
-set(CPU_FLAGS "--target=arm-arm-none-eabi -mcpu=cortex-m4 -march=armv7e-m")
+set(CPU_FLAGS "--target=arm-arm-none-eabi -mthumb -mcpu=cortex-m4 -march=armv7e-m")
+set(VFP_FLAGS "-mfloat-abi=hard -mfpu=fpv4-sp-d16")
 
 include(${CMAKE_CURRENT_LIST_DIR}/arm-cortex-keil-toolchain.cmake)
